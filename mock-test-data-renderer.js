000806fd2f9a05b28ef8288f934974fff02c7ac5
vinu216 <vinulol925@gmail.com>
2026-06-01 09:35:27 +0530

    Fix PTET mock test 6 rendering

--- mock-test-data-renderer.js
+++ mock-test-data-renderer.js
@@ -277,6 +277,8 @@
           ? './ptet-mock-test-7.html'
           : teachingExamSlug === 'ptet' && testNumber === 8
           ? './ptet-mock-test-8.html'
+          : teachingExamSlug === 'ptet' && testNumber === 9
+          ? './ptet-mock-test-9.html'
           : teachingExamSlug === 'bstc' && testNumber === 1
           ? './bstc-mock-test-1.html'
           : teachingExamSlug === 'bstc' && testNumber === 2
