00174f5e6d79a177ad92619551b288f9fabbe4de
vinu216 <vinulol925@gmail.com>
2026-06-14 20:42:53 +0530

    Update REET Mock Test 6 questions 141-150

--- mock-test-data-renderer.js
+++ mock-test-data-renderer.js
@@ -310,6 +310,8 @@
           ? './reet-mock-test-4.html'
           : teachingExamSlug === 'reet' && testNumber === 5
           ? './reet-mock-test-5.html'
+          : teachingExamSlug === 'reet' && testNumber === 6
+          ? './reet-mock-test-6.html'
           : teachingExamSlug === 'bstc' && testNumber === 1
           ? './bstc-mock-test-1.html'
           : teachingExamSlug === 'bstc' && testNumber === 2
