254026b4e3b6def3336d79eba3d052929db14196
vinu216 <vinulol925@gmail.com>
2026-06-19 12:40:28 +0530

    Update REET mock test 9 questions 141 to 150 (#235)

--- mock-test-data-renderer.js
+++ mock-test-data-renderer.js
@@ -316,6 +316,8 @@
           ? './reet-mock-test-7.html'
           : teachingExamSlug === 'reet' && testNumber === 8
           ? './reet-mock-test-8.html'
+          : teachingExamSlug === 'reet' && testNumber === 9
+          ? './reet-mock-test-9.html'
           : teachingExamSlug === 'bstc' && testNumber === 1
           ? './bstc-mock-test-1.html'
           : teachingExamSlug === 'bstc' && testNumber === 2
