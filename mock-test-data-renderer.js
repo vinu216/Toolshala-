f1b8d4bc0f1810f8e9bb037f2a7ed83d774d7c89
vinu216 <vinulol925@gmail.com>
2026-07-05 07:38:25 +0530

    Update CTET mock test 7 questions 31-40

--- mock-test-data-renderer.js
+++ mock-test-data-renderer.js
@@ -337,6 +337,8 @@
           ? './ctet-mock-test-4.html'
           : teachingExamSlug === 'ctet' && testNumber === 5
           ? './ctet-mock-test-5.html'
+          : teachingExamSlug === 'ctet' && testNumber === 7
+          ? './ctet-mock-test-7.html'
           : teachingExamSlug === 'reet' && testNumber === 1
           ? './reet-mock-test-1.html'
           : teachingExamSlug === 'reet' && testNumber === 2
@@ -380,7 +382,7 @@
               : `../exam.html?exam=${exam.slug}`;
         return cardForExam({
           ...exam,
-          ...(teachingExamSlug === 'ctet' && testNumber >= 1 && testNumber <= 5 ? { questionsCount: 150, duration: '150 min' } : {}),
+          ...(teachingExamSlug === 'ctet' && ((testNumber >= 1 && testNumber <= 5) || testNumber === 7) ? { questionsCount: 150, duration: '150 min' } : {}),
           ctaLink: customCtaLink,
           title: `${exam.title} Mock Test ${testNumber}`,
           description: teachingExamSlug === 'bstc'
