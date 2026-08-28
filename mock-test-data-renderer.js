60fe0a8b86173e3bb59600adbec487fa14248e8e
Vinod <vinulol925@gmail.com>
2026-08-28 21:25:00 +0530

    Update mock-test-data-renderer.js

--- mock-test-data-renderer.js
+++ mock-test-data-renderer.js
@@ -322,6 +322,8 @@
           ? './3rd-grade-mock-test-3.html'
           : teachingExamSlug === '3rd-grade' && testNumber === 4
           ? './3rd-grade-mock-test-4.html'
+          : teachingExamSlug === '3rd-grade' && testNumber === 4
+          ? './3rd-grade-mock-test-5.html'
           : teachingExamSlug === 'ptet' && testNumber === 1
           ? './ptet-mock-test-1.html'
           : teachingExamSlug === 'ptet' && testNumber === 2
